0b7c325c59fd8940329386cf494949be6cefe3bc
giowitlidze <giorgig31@gmail.com>
2017-06-28 17:54:21 +0400

    Update Scraper.java

--- src/Scraper.java
+++ src/Scraper.java
@@ -6,14 +6,14 @@ public class Scraper {
 
     public static void main(String[] args) throws Exception {
 
-        final Document document = Jsoup.connect("http://www.imdb.com/chart/top").get();
+         final Document document = Jsoup.connect("http://www.imdb.com/chart/top").get();
 
         for (Element row : document.select("table.chart.full-width tr")) {
 
             final String title = row.select(".titleColumn a").text();
-            final String rating = row.select(".imdbRating").text();
 
-            System.out.println(title + " -> Rating: " + rating);
+
+            System.out.println(title);
         }
     }
 }
